b380b9cf915533a77ed54501a4cf32a45912fb48
sudo-rickroll <rangasai.kr@gmail.com>
2025-01-07 03:21:59 -0500

    Add lint

--- index.js
+++ index.js
@@ -1,92 +1,92 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
 const Phonebook = require('./models/phonebook.js')
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static('dist'));
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
 
 
 morgan.token('content', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 const unknownEndpointHandler = (request, response, next) => next({
-    message: "Unknown Endpoint"
-});
+  message: 'Unknown Endpoint'
+})
 
 const errorHandler = (error, request, response, next) => {
-    
-    response.status(500).send({
-        error: error.message
-    })
-    
+
+  response.status(500).send({
+    error: error.message
+  })
+
 }
 
-app.get("/api/persons", (request, response, next) => {
-    Phonebook.find({}).then(result => response.json(result)).catch(error => next(error));
+app.get('/api/persons', (request, response, next) => {
+  Phonebook.find({}).then(result => response.json(result)).catch(error => next(error))
 })
 
-app.get("/info", (request, response, next) => {
-    Phonebook.find({}).then(result => response.send(`<p>Phonebook has info for ${result.length} people</p><br />    ${new Date()}`)).catch(error => next(error));    
+app.get('/info', (request, response, next) => {
+  Phonebook.find({}).then(result => response.send(`<p>Phonebook has info for ${result.length} people</p><br />    ${new Date()}`)).catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response, next) => {
-    Phonebook.findById(request.params.id.toString()).then(result => {
-        response.json(result);
-    }).catch(error => next(error))
+app.get('/api/persons/:id', (request, response, next) => {
+  Phonebook.findById(request.params.id.toString()).then(result => {
+    response.json(result)
+  }).catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response, next) => {
-    const item = Phonebook.findByIdAndDelete(request.params.id)
-                            .then(result => {
-                                if(result === null)
-                                {
-                                    next({
-                                        message: "Item not found, may have been deleted already"
-                                    })                                    
-                                }
-                                else{
-                                    response.status(200).json(result);
-                                }
-                            })
-                            .catch(error => next(error))
+app.delete('/api/persons/:id', (request, response, next) => {
+  Phonebook.findByIdAndDelete(request.params.id)
+    .then(result => {
+      if(result === null)
+      {
+        next({
+          message: 'Item not found, may have been deleted already'
+        })
+      }
+      else{
+        response.status(200).json(result)
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response, next) => {
-    const contact = new Phonebook(request.body);
+app.post('/api/persons', (request, response, next) => {
+  const contact = new Phonebook(request.body)
 
-    Phonebook.find({name: contact.name}).then(result => {
-        if(result.length > 0){
-            response.status(400).json({
-                message: "Name already exists"
-            })
-        }else{
-            contact.save().then(result => {
-                response.status(200).json(result);
-            }).catch(error => next(error))
-        }
+  Phonebook.find({ name: contact.name }).then(result => {
+    if(result.length > 0){
+      response.status(400).json({
+        message: 'Name already exists'
+      })
+    }else{
+      contact.save().then(result => {
+        response.status(200).json(result)
+      }).catch(error => next(error))
+    }
 
-    }).catch(error => next(error))
-    
+  }).catch(error => next(error))
+
 })
 
-app.put("/api/persons/:id", (request, response, next) => {
-    let contact = request.body;
-    
+app.put('/api/persons/:id', (request, response, next) => {
+  let contact = request.body
 
-    Phonebook.findByIdAndUpdate(request.params.id, contact, {new: true})
-                .then(result => response.json(result))
-                .catch(error => next(error))
+
+  Phonebook.findByIdAndUpdate(request.params.id, contact, { new: true })
+    .then(result => response.json(result))
+    .catch(error => next(error))
 })
 
-app.use(unknownEndpointHandler);
-app.use(errorHandler);
+app.use(unknownEndpointHandler)
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Application started on ${PORT}`);
+  console.log(`Application started on ${PORT}`)
 })
--- models/phonebook.js
+++ models/phonebook.js
@@ -1,41 +1,41 @@
 require('dotenv').config()
-const mongoose = require('mongoose');
-const fs = require('fs');
+const mongoose = require('mongoose')
+const fs = require('fs')
 
 const validationConstraints = {
-    numberRegex: /^\d{2,3}-\d{6,}/,
-    message: input => `Phone number ${input} does not meet the format specifications`,
-    validator: input => validationConstraints.numberRegex.test(input)
+  numberRegex: /^\d{2,3}-\d{6,}/,
+  message: input => `Phone number ${input} does not meet the format specifications`,
+  validator: input => validationConstraints.numberRegex.test(input)
 }
 
-const url = process.env.MONGODB_URI || fs.readFileSync('/etc/secrets/MONGODB_URI', 'utf8').trim();
+const url = process.env.MONGODB_URI || fs.readFileSync('/etc/secrets/MONGODB_URI', 'utf8').trim()
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 const schema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        required: true,
-        validate:{
-            validator: input => validationConstraints.validator(input),
-            message: input => validationConstraints.message(input.value)
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate:{
+      validator: input => validationConstraints.validator(input),
+      message: input => validationConstraints.message(input.value)
     }
+  }
 })
 
 schema.set('toJSON', {
-    transform: (document, renderedObject) => {
-        renderedObject.id = renderedObject._id.toString();
-        delete renderedObject._id;
-        delete renderedObject.__v;
-    }
+  transform: (document, renderedObject) => {
+    renderedObject.id = renderedObject._id.toString()
+    delete renderedObject._id
+    delete renderedObject.__v
+  }
 })
 
-module.exports = mongoose.model('Phonebook', schema);
+module.exports = mongoose.model('Phonebook', schema)
 
 
